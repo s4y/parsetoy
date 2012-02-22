da3341da1fc8ade2a022a12fbb37ae4b912575c9
Sidney San Martín <s@sidneysm.com>
2012-02-21 20:44:20 -0500

    Use declared rules, not hard-coded if/else

--- parsetoy.py
+++ parsetoy.py
@@ -1,6 +1,12 @@
 from collections import namedtuple
 
 Token = namedtuple('Token', ( 'type', 'value' ))
+Rule = namedtuple('Rule', ( 'operator', 'inputs', 'handler' ))
+
+rules = (
+	Rule(None, ( 'number', ), lambda n: Token('expression', n.value)),
+	Rule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) )
+)
 
 tokens = [
 	Token('number', 10),
@@ -11,20 +17,18 @@ tokens = [
 stack  = []
 
 while True:
-	if len(stack) and stack[-1].type == 'number':
-		stack[-1] = Token('expression', stack[-1].value)
-	elif len(stack) >= 3 and stack[-3].type == 'expression' and stack[-2].type == '+' and stack [-1].type == 'expression':
-		r = stack.pop();
-		stack.pop();
-		l = stack.pop();
-		stack.append(Token('expression', l.value + r.value))
-	elif len(tokens):
-		stack.append(tokens[0])
-		del tokens[0]
-	elif len(stack) == 1:
-		break
+	for rule in rules:
+		if len(stack) >= len(rule.inputs) and all( a == b for a, b in zip(rule.inputs, (t.type for t in stack[-len(rule.inputs):]))):
+			stack[-len(rule.inputs):] = [rule.handler(*stack[-len(rule.inputs):])]
+			break
 	else:
-		print "Parse error"
-		break
+		if len(tokens):
+			stack.append(tokens[0])
+			del tokens[0]
+		elif len(stack) == 1:
+			break
+		else:
+			print "Parse error"
+			break
 
 print 'Stack: %s' % (stack,)
