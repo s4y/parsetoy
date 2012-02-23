ec3e4d9f8d6ae397d92415e406e569de77d6e4c9
Sidney San Martín <s@sidneysm.com>
2012-02-23 11:00:48 -0500

    Refactor parser into function, add lexer
    FWIW, I’m not sure why exactly we have "number"s as well as "expression"s

--- parsetoy.py
+++ parsetoy.py
@@ -1,34 +1,62 @@
+# encoding=utf-8
+
 from collections import namedtuple
+import re, sys
 
 Token = namedtuple('Token', ( 'type', 'value' ))
-Rule = namedtuple('Rule', ( 'operator', 'inputs', 'handler' ))
+LexRule = namedtuple('LexRule', ( 'expression', 'handler' ))
+ParseRule = namedtuple('ParseRule', ( 'operator', 'inputs', 'handler' ))
 
-rules = (
-	Rule(None, ( 'number', ), lambda n: Token('expression', n.value)),
-	Rule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) )
-)
-
-tokens = [
-	Token('number', 10),
-	Token('+', None),
-	Token('number', 3),
-	Token('+', None), Token('number', 5)
-]
-stack  = []
-
-while True:
-	for rule in rules:
-		if len(stack) >= len(rule.inputs) and all( a == b for a, b in zip(rule.inputs, (t.type for t in stack[-len(rule.inputs):]))):
-			stack[-len(rule.inputs):] = [rule.handler(*stack[-len(rule.inputs):])]
-			break
-	else:
-		if len(tokens):
-			stack.append(tokens[0])
-			del tokens[0]
-		elif len(stack) == 1:
-			break
+def lex(grammar, input):
+	while len(input):
+		for rule in grammar:
+			match = rule.expression.match(input)
+			if match:
+				matchText = match.group()
+				token = rule.handler(matchText)
+				if token:
+					yield token
+				input = input[len(matchText):]
+				break
 		else:
-			print "Parse error"
-			break
+			raise Exception('Couldn’t parse your input')
 
-print 'Stack: %s' % (stack,)
+
+def parse(rules, tokens):
+	stack = []
+	while True:
+		for rule in rules:
+			if len(stack) >= len(rule.inputs) and all( a == b for a, b in zip(rule.inputs, (t.type for t in stack[-len(rule.inputs):]))):
+				stack[-len(rule.inputs):] = [rule.handler(*stack[-len(rule.inputs):])]
+				break
+		else:
+			if len(tokens):
+				stack.append(tokens[0])
+				del tokens[0]
+			elif len(stack) == 1:
+				break
+			else:
+				# Should be an exception, but want to see the stack for now
+				print "Parse error"
+				break
+	return stack
+
+
+if len(sys.argv) != 2:
+	print 'usage: %s pattern' % (sys.argv[0],)
+	sys.exit(1);
+
+tokens = list(lex((
+	LexRule(re.compile(r'\s+'), lambda match: None),
+	LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:.[0-9]+)?)'), lambda match: Token('number', float(match))),
+	LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
+	LexRule(re.compile(r'-'), lambda match: Token('-', None))
+), sys.argv[1]))
+
+print tokens
+
+print parse((
+	ParseRule(None, ( 'number', ), lambda n: Token('expression', n.value)),
+	ParseRule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) ),
+	ParseRule('-', ( 'expression', '-', 'expression' ), lambda l, op, r: Token('expression', l.value - r.value) )
+), tokens)
