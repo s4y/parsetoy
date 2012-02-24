d45b99697d74a2c4d8b9fc95e53eb3807ea9185f
Sidney San Martín <s@sidneysm.com>
2012-02-24 18:57:15 -0500

    Move grammar up into a CalculatorGrammar class with class variables

--- parsetoy.py
+++ parsetoy.py
@@ -7,6 +7,23 @@ import re
 Token = namedtuple('Token', ( 'type', 'value' ))
 LexRule = namedtuple('LexRule', ( 'expression', 'handler' ))
 ParseRule = namedtuple('ParseRule', ( 'operator', 'inputs', 'handler' ))
+OpPrecedence = namedtuple('OpPrecedence', ( 'associativity', 'operators' ))
+
+class CalculatorGrammar:
+	grammar = (
+		LexRule(re.compile(r'\s+'), lambda match: None),
+		LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)'), lambda match: Token('number', float(match))),
+		LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
+		LexRule(re.compile(r'-'), lambda match: Token('-', None)),
+		LexRule(re.compile(r'\('), lambda match: Token('(', None)),
+		LexRule(re.compile(r'\)'), lambda match: Token(')', None))
+	)
+	expressions = (
+		ParseRule(None, ( 'number', ), lambda n: Token('expression', n.value)),
+		ParseRule(None, ( '(', 'expression', ')' ), lambda l, ex, r: ex ),
+		ParseRule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) ),
+		ParseRule('-', ( 'expression', '-', 'expression' ), lambda l, op, r: Token('expression', l.value - r.value) )
+	)
 
 class LexError(Exception):
 	"""Input couldn't be lexed based on this grammer."""
@@ -14,16 +31,7 @@ class LexError(Exception):
 
 def lex(grammar, input):
 	"""Generate a series of tokens from an input string based on a grammar.
-
-		>>> grammar = (
-		...		LexRule(re.compile(r'\s+'), lambda match: None),
-		...     LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)'), lambda match: Token('number', float(match))),
-		...     LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
-		...     LexRule(re.compile(r'-'), lambda match: Token('-', None)),
-		...     LexRule(re.compile(r'\('), lambda match: Token('(', None)),
-		...     LexRule(re.compile(r'\)'), lambda match: Token(')', None))
-		... )
-		>>> list(lex(grammar, '1 + 5 +7 - 0.5'))
+		>>> list(lex(CalculatorGrammar.grammar, '1 + 5 +7 - 0.5'))
 		[Token(type='number', value=1.0), Token(type='+', value=None), Token(type='number', value=5.0), Token(type='+', value=None), Token(type='number', value=7.0), Token(type='-', value=None), Token(type='number', value=0.5)]
 	"""
 	while input:
@@ -69,14 +77,7 @@ def test():
 def main():
 	import sys
 	try:
-		tokens = list(lex((
-			LexRule(re.compile(r'\s+'), lambda match: None),
-			LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)'), lambda match: Token('number', float(match))),
-			LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
-			LexRule(re.compile(r'-'), lambda match: Token('-', None)),
-			LexRule(re.compile(r'\('), lambda match: Token('(', None)),
-			LexRule(re.compile(r'\)'), lambda match: Token(')', None))
-		), sys.argv[1]))
+		tokens = list(lex(CalculatorGrammar.grammar, sys.argv[1]))
 	except LexError:
 		print('Couldn’t lex your input')
 		sys.exit(2)
@@ -87,12 +88,7 @@ def main():
 	print('')
 
 	try:
-		parsed = parse((
-			ParseRule(None, ( 'number', ), lambda n: Token('expression', n.value)),
-			ParseRule(None, ( '(', 'expression', ')' ), lambda l, ex, r: ex ),
-			ParseRule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) ),
-			ParseRule('-', ( 'expression', '-', 'expression' ), lambda l, op, r: Token('expression', l.value - r.value) )
-		), tokens)
+		parsed = parse(CalculatorGrammar.expressions, tokens)
 	except ParseError as e:
 		print('Parse error. Stack:')
 		print(e.stack)
