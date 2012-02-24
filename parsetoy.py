acceb36524fc00eda4360c899fb24da7c5f2c2df
Sidney San Martín <s@sidneysm.com>
2012-02-24 18:24:07 -0500

    `len()` not needed, add docstrings, start using doctest, restructuring — thanks James!

--- parsetoy.py
+++ parsetoy.py
@@ -2,17 +2,31 @@
 # encoding=utf-8
 
 from collections import namedtuple
-import re, sys
+import re
 
 Token = namedtuple('Token', ( 'type', 'value' ))
 LexRule = namedtuple('LexRule', ( 'expression', 'handler' ))
 ParseRule = namedtuple('ParseRule', ( 'operator', 'inputs', 'handler' ))
 
 class LexError(Exception):
+	"""Input couldn't be lexed based on this grammer."""
 	pass
 
 def lex(grammar, input):
-	while len(input):
+	"""Generate a series of tokens from an input string based on a grammar.
+
+		>>> grammar = (
+		...		LexRule(re.compile(r'\s+'), lambda match: None),
+		...     LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)'), lambda match: Token('number', float(match))),
+		...     LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
+		...     LexRule(re.compile(r'-'), lambda match: Token('-', None)),
+		...     LexRule(re.compile(r'\('), lambda match: Token('(', None)),
+		...     LexRule(re.compile(r'\)'), lambda match: Token(')', None))
+		... )
+		>>> list(lex(grammar, '1 + 5 +7 - 0.5'))
+		[Token(type='number', value=1.0), Token(type='+', value=None), Token(type='number', value=5.0), Token(type='+', value=None), Token(type='number', value=7.0), Token(type='-', value=None), Token(type='number', value=0.5)]
+	"""
+	while input:
 		for rule in grammar:
 			match = rule.expression.match(input)
 			if match:
@@ -27,6 +41,7 @@ def lex(grammar, input):
 
 
 class ParseError(Exception):
+	"""Input couldn't be parsed under these rules"""
 	def __init__(self, stack):
 		self.stack = stack
 
@@ -47,44 +62,56 @@ def parse(rules, tokens):
 				raise ParseError(stack)
 	return stack
 
+def test():
+	import doctest
+	return doctest.testmod()
 
-if len(sys.argv) != 2:
-	print 'usage: %s pattern' % (sys.argv[0],)
-	sys.exit(1);
+def main():
+	import sys
+	try:
+		tokens = list(lex((
+			LexRule(re.compile(r'\s+'), lambda match: None),
+			LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)'), lambda match: Token('number', float(match))),
+			LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
+			LexRule(re.compile(r'-'), lambda match: Token('-', None)),
+			LexRule(re.compile(r'\('), lambda match: Token('(', None)),
+			LexRule(re.compile(r'\)'), lambda match: Token(')', None))
+		), sys.argv[1]))
+	except LexError:
+		print('Couldn’t lex your input')
+		sys.exit(2)
 
-try:
-	tokens = list(lex((
-		LexRule(re.compile(r'\s+'), lambda match: None),
-		LexRule(re.compile(r'(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)'), lambda match: Token('number', float(match))),
-		LexRule(re.compile(r'\+'), lambda match: Token('+', None)),
-		LexRule(re.compile(r'-'), lambda match: Token('-', None)),
-		LexRule(re.compile(r'\('), lambda match: Token('(', None)),
-		LexRule(re.compile(r'\)'), lambda match: Token(')', None))
-	), sys.argv[1]))
-except LexError:
-	print 'Couldn’t lex your input'
-	sys.exit(2)
+	print('Lexer results:')
+	print(tokens)
 
-print 'Lexer results:'
-print tokens
+	print('')
 
-print ''
+	try:
+		parsed = parse((
+			ParseRule(None, ( 'number', ), lambda n: Token('expression', n.value)),
+			ParseRule(None, ( '(', 'expression', ')' ), lambda l, ex, r: ex ),
+			ParseRule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) ),
+			ParseRule('-', ( 'expression', '-', 'expression' ), lambda l, op, r: Token('expression', l.value - r.value) )
+		), tokens)
+	except ParseError as e:
+		print('Parse error. Stack:')
+		print(e.stack)
 
-try:
-	parsed = parse((
-		ParseRule(None, ( 'number', ), lambda n: Token('expression', n.value)),
-		ParseRule(None, ( '(', 'expression', ')' ), lambda l, ex, r: ex ),
-		ParseRule('+', ( 'expression', '+', 'expression' ), lambda l, op, r: Token('expression', l.value + r.value) ),
-		ParseRule('-', ( 'expression', '-', 'expression' ), lambda l, op, r: Token('expression', l.value - r.value) )
-	), tokens)
-except ParseError as e:
-	print 'Parse error. Stack:'
-	print e.stack
+	else:
+		print('Parser results:')
+		print(parsed)
 
-else:
-	print 'Parser results:'
-	print parsed
+		if any(parsed):
+			print('\nValue:')
+			print(parsed[-1].value)
 
-	if any(parsed):
-		print '\nValue:'
-		print parsed[-1].value
+
+if __name__ == "__main__":
+	import sys
+	if len(sys.argv) == 1:
+		test()
+		sys.exit()
+	if len(sys.argv) != 2:
+		print('usage: %s pattern' % (sys.argv[0],))
+		sys.exit(1)
+	main()
