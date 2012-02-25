6cb1cc7b08129d6612f025e3afa21d69ab41ee7d
Sidney San Martín <s@sidneysm.com>
2012-02-24 20:34:51 -0500

    Refactor parser, lexer into classes

--- parsetoy.py
+++ parsetoy.py
@@ -25,50 +25,53 @@ class CalculatorGrammar:
 		ParseRule('-', ( 'expression', '-', 'expression' ), lambda l, op, r: Token('expression', l.value - r.value) )
 	)
 
-class LexError(Exception):
-	"""Input couldn't be lexed based on this grammer."""
-	pass
 
-def lex(grammar, input):
-	"""Generate a series of tokens from an input string based on a grammar.
-		>>> list(lex(CalculatorGrammar.grammar, '1 + 5 +7 - 0.5'))
-		[Token(type='number', value=1.0), Token(type='+', value=None), Token(type='number', value=5.0), Token(type='+', value=None), Token(type='number', value=7.0), Token(type='-', value=None), Token(type='number', value=0.5)]
-	"""
-	while input:
-		for rule in grammar:
-			match = rule.expression.match(input)
-			if match:
-				matchText = match.group()
-				token = rule.handler(matchText)
-				if token:
-					yield token
-				input = input[len(matchText):]
-				break
-		else:
-			raise LexError
-
-
-class ParseError(Exception):
-	"""Input couldn't be parsed under these rules"""
-	def __init__(self, stack):
-		self.stack = stack
-
-def parse(rules, tokens):
-	stack = []
-	while True:
-		for rule in rules:
-			if len(stack) >= len(rule.inputs) and all( a == b for a, b in zip(rule.inputs, (t.type for t in stack[-len(rule.inputs):]))):
-				stack[-len(rule.inputs):] = [rule.handler(*stack[-len(rule.inputs):])]
-				break
-		else:
-			if len(tokens):
-				stack.append(tokens[0])
-				del tokens[0]
-			elif len(stack) <= 1:
-				break
+class Lexer:
+	class LexError(Exception):
+		"""Input couldn't be lexed based on this grammar."""
+	def __init__(self, grammar):
+		self.grammar = grammar
+	def lex(self, input):
+		"""Generate a series of tokens from an input string based on a grammar.
+			>>> list(lex(CalculatorGrammar.grammar, '1 + 5 +7 - 0.5'))
+			[Token(type='number', value=1.0), Token(type='+', value=None), Token(type='number', value=5.0), Token(type='+', value=None), Token(type='number', value=7.0), Token(type='-', value=None), Token(type='number', value=0.5)]
+		"""
+		while input:
+			for rule in self.grammar:
+				match = rule.expression.match(input)
+				if match:
+					matchText = match.group()
+					token = rule.handler(matchText)
+					if token:
+						yield token
+					input = input[len(matchText):]
+					break
 			else:
-				raise ParseError(stack)
-	return stack
+				raise Lexer.LexError
+
+class Parser:
+	class ParseError(Exception):
+		"""Input couldn't be parsed under these rules"""
+		def __init__(self, stack):
+			self.stack = stack
+	def __init__(self, rules):
+		self.rules = rules
+	def parse(self, tokens):
+		stack = []
+		while True:
+			for rule in self.rules:
+				if len(stack) >= len(rule.inputs) and all( a == b for a, b in zip(rule.inputs, (t.type for t in stack[-len(rule.inputs):]))):
+					stack[-len(rule.inputs):] = [rule.handler(*stack[-len(rule.inputs):])]
+					break
+			else:
+				if len(tokens):
+					stack.append(tokens[0])
+					del tokens[0]
+				elif len(stack) <= 1:
+					break
+				else:
+					raise Parser.ParseError(stack)
+		return stack
 
 def test():
 	import doctest
@@ -76,9 +79,13 @@ def test():
 
 def main():
 	import sys
+
+	parser = Parser(CalculatorGrammar.expressions)
+	lexer = Lexer(CalculatorGrammar.grammar)
+
 	try:
-		tokens = list(lex(CalculatorGrammar.grammar, sys.argv[1]))
-	except LexError:
+		tokens = list(lexer.lex(sys.argv[1]))
+	except Lexer.LexError:
 		print('Couldn’t lex your input')
 		sys.exit(2)
 
@@ -88,8 +95,8 @@ def main():
 	print('')
 
 	try:
-		parsed = parse(CalculatorGrammar.expressions, tokens)
-	except ParseError as e:
+		parsed = parser.parse(tokens)
+	except Parser.ParseError as e:
 		print('Parse error. Stack:')
 		print(e.stack)
 
