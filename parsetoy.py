d5e51104e7fb89d10f7eaf540e0e851d42819702
Sidney San Martín <s@sidneysm.com>
2012-02-24 20:35:18 -0500

    Minor refactor of code in __name__ == "__main__"

--- parsetoy.py
+++ parsetoy.py
@@ -113,8 +113,8 @@ if __name__ == "__main__":
 	import sys
 	if len(sys.argv) == 1:
 		test()
-		sys.exit()
-	if len(sys.argv) != 2:
+	elif len(sys.argv) != 2:
 		print('usage: %s pattern' % (sys.argv[0],))
 		sys.exit(1)
-	main()
+	else:
+		main()
